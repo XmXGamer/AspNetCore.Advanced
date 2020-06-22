1b8eeea1b00b2532762c00a87e208ce585dd6983
Marcel Barner <marcel.barner@aitgmbh.de>
2020-06-22 09:25:57 +0200

    fix tests

--- src/SimpleAuthentication/SimpleAuthentication.ExampleApp/Startup.cs
+++ src/SimpleAuthentication/SimpleAuthentication.ExampleApp/Startup.cs
@@ -26,8 +26,6 @@ namespace SecretAuthentication.ExampleApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddAuthentication(SecretAuthenticationDefaults.AuthenticationSchema)
-                .AddSecretAuthentication(opt => opt.Secret = "Example");
             services.AddControllers();
         }
 
